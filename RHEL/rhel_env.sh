d8413df88fdfa1c6140a450ca492535eae438dfe
dan-drago <ddarg@umd.edu>
2020-07-10 16:49:07 -0400

    Tweaks

--- RHEL/rhel_env.sh
+++ RHEL/rhel_env.sh
@@ -5,5 +5,7 @@
 ##############################################################
 
 ### Path for yusr-installed executables
-[[ -d $HOME/.yusr ]] && export PATH="$HOME/.yusr/sbin:$HOME/.yusr/bin:$PATH"
+[[ -d $HOME/.yusr ]] && export PATH="$HOME/.yusr/sbin:$HOME/.yusr/bin:$HOME/.yusr/usr/bin:$HOME/.yusr/usr/bin:$PATH"
 
+
+[[ -d $HOME/.yusr ]] && L='/lib:/lib64:/usr/lib:/usr/lib64' && export LD_LIBRARY_PATH="$L:$HOME/.yusr/usr/lib:$HOME/.yusr/usr/lib64"
