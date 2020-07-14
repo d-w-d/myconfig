e736c6e30e4db173070b7866fcd5c9af83a5044a
dan-drago <ddarg@umd.edu>
2020-07-14 15:56:32 -0400

    Tweaks

--- RHEL/source_rhel.sh
+++ RHEL/source_rhel.sh
@@ -8,4 +8,5 @@ MYCONFIG_ROOT_DIR=${MYCONFIG_ROOT_DIR:-"$HOME/.myconfig"}
 ### Source 'em:
 source $MYCONFIG_ROOT_DIR/RHEL/rhel_aliases.sh
 source $MYCONFIG_ROOT_DIR/RHEL/rhel_env.sh
+source $MYCONFIG_ROOT_DIR/RHEL/rhel_functions.sh
 source $MYCONFIG_ROOT_DIR/RHEL/rhel_git.sh
