2d62cb1b5e0c9e8509730ae073b948ec9aaf1654
dan-drago <ddarg@umd.edu>
2020-07-20 10:19:53 -0400

    Tweaks

--- tmp_install.sh
+++ tmp_install.sh
@@ -77,6 +77,10 @@ else
     fi
 fi
 
+########################################################
+# Provide function to fully install myconfig to home dir
+########################################################
+
 myconfig_full_installation() {
     echo 'Running full installation'
     source /tmp/myconfig/perm_install.sh
@@ -84,24 +88,13 @@ myconfig_full_installation() {
 export -f myconfig_full_installation
 
 ###############################################
-# Create temp bashrc amalgum file and source it
-###############################################
 
-TEMPFILE=$(mktemp)
-cat /tmp/myconfig/entry.sh >$TEMPFILE
-echo "alias vim='vim -N -u /tmp/myconfig/.vimrc'" >>$TEMPFILE
-echo "source /tmp/myconfig/UTILS/color_params.sh" >>$TEMPFILE
+source /tmp/myconfig/entry.sh
+alias vim='vim -N -u /tmp/myconfig/.vimrc'
 
-#echo "
-#myconfig_full_installation(){
-    #echo 'Running full installation'
-    #source /tmp/myconfig/perm_install.sh
-#}
-#export -f myconfig_full_installation
-#" >>$TEMPFILE
 
-echo "
-echo -e '${CYA}
+
+echo -e  """
 ===================
 MYCONFIG DOWNLOADED
 ===================
@@ -113,13 +106,45 @@ ${GRE}
 - Vim status: ${WHI}$(type vim 2>&1)${GRE}
 
 - Run${RED} myconfig_full_installation${GRE} for full install.
+"""
 
-'
-" >>$TEMPFILE
-cat $TEMPFILE >temp.sh
 
-### Source temp bashrc file
-MYCONFIG_ROOT_DIR='/tmp/myconfig' source $TEMPFILE
+# Create temp bashrc amalgum file and source it
+###############################################
 
-### Return to original dir
-cd $PREVIOUSDIR
+#TEMPFILE=$(mktemp)
+#cat /tmp/myconfig/entry.sh >$TEMPFILE
+#echo "alias vim='vim -N -u /tmp/myconfig/.vimrc'" >>$TEMPFILE
+#echo "source /tmp/myconfig/UTILS/color_params.sh" >>$TEMPFILE
+
+#echo "
+#myconfig_full_installation(){
+    #echo 'Running full installation'
+    #source /tmp/myconfig/perm_install.sh
+#}
+#export -f myconfig_full_installation
+#" >>$TEMPFILE
+
+#echo "
+#echo -e '${CYA}
+#===================
+#MYCONFIG DOWNLOADED
+#===================
+#${GRE}
+#- Configuration scripts have been cloned to ${WHI}/tmp/myconfig${GRE}
+
+#- Bash has sourced ${WHI}/tmp/myconfig/entry.sh${GRE}
+
+#- Vim status: ${WHI}$(type vim 2>&1)${GRE}
+
+#- Run${RED} myconfig_full_installation${GRE} for full install.
+
+#'
+#" >>$TEMPFILE
+#cat $TEMPFILE >temp.sh
+
+#### Source temp bashrc file
+#MYCONFIG_ROOT_DIR='/tmp/myconfig' source $TEMPFILE
+
+#### Return to original dir
+#cd $PREVIOUSDIR
