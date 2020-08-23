4b0d09d19a027a9f80b96653b0550853a4dc9e14
dan-drago <ddarg@umd.edu>
2020-08-23 12:08:03 -0400

    Tweaks

--- COMMON/common_iterm2.sh
+++ COMMON/common_iterm2.sh
@@ -2,7 +2,7 @@
 
 export ITERM_ENABLE_SHELL_INTEGRATION_WITH_TMUX=1
 
-[[ $BASH ]] && [[ -f $HOME/.iterm2_shell_integration.bash ]] && source $HOME/.iterm2_shell_integration.bash
+[ $BASH ] && [ -f $HOME/.iterm2_shell_integration.bash ] && source $HOME/.iterm2_shell_integration.bash
 
-[[ $(echo $SHELL | grep zsh | wc -l) ]] && [[ -f $HOME/.iterm2_shell_integration.zsh ]] && source $HOME/.iterm2_shell_integration.zsh
+[ $(echo $SHELL | grep zsh | wc -l) ] && [ -f $HOME/.iterm2_shell_integration.zsh ] && source $HOME/.iterm2_shell_integration.zsh
 
