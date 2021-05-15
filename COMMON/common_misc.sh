1fa7c31a3f05db065eb876a4a0000ac471a85474
d-w-d <ddarg@umd.edu>
2021-05-15 16:08:27 -0400

    Update nvm infrastructure; use even versions by default

---
+++ COMMON/common_misc.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env false
+
+### Odd versions of node are not LTS and not suitable for production
+### So use the latest even version number as default
+nvm alias default 14
--- COMMON/common_nvm.sh
+++ COMMON/common_nvm.sh
@@ -13,25 +13,5 @@ export NVM_DIR="$HOME/.nvm"
 
 source "$NVM_DIR"/nvm.sh
 
-### Adapted from: https://www.growingwiththeweb.com/2018/01/slow-nvm-init.html
-#if [ -s "$HOME/.nvm/nvm.sh" ] && [ ! "$(fun_data_type __init_nvm)" = function ]; then
-#export NVM_DIR="$HOME/.nvm"
-#[ -s "$NVM_DIR/bash_completion" ] && source "$NVM_DIR/bash_completion"
-#declare -a __node_commands=('nvm' 'node' 'npm' 'yarn' 'gulp' 'grunt' 'webpack')
-#function __init_nvm() {
-#for i in "${__node_commands[@]}"; do unalias $i; done
-#source "$NVM_DIR"/nvm.sh
-#unset __node_commands
-#unset -f __init_nvm
-#}
-#for i in "${__node_commands[@]}"; do alias $i='__init_nvm && '$i; done
-
-#export NODE_PATH="$(npm config get prefix)/lib/node_modules"
-#fi
-
-# Safety measure recommended by this dude: https://youtu.be/24tQRwIRP_w?t=961
-# npm config set ignore-scripts true    # "safe" but pain-in-derriere
-#npm config set ignore-scripts false # "unsafe" but practical
-
 # Add node executables globally installed by nvm-enables npm
 export PATH="$PATH:$NVM_BIN"
