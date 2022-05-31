2f2c9c5f53a21410593ecc5c199eca778781e17c
d-w-d <ddarg@umd.edu>
2022-05-31 12:34:36 -0400

    Some improvements

--- DEBIAN/debian_aliases.sh
+++ DEBIAN/debian_aliases.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env false
 
-
-alias ls='ls -F --color'        # Sort with color and file-type indicator
+alias ls='ls -F --color' # Sort with color and file-type indicator
 alias allsblk="lsblk -o name,fstype,size,mountpoint,partuuid"
 alias alportsinuse='sudo netstat -tulpn'
 alias alwwwhtml='cd /var/www/html'
 
+# Opens a file with default application on linux
+alias open='xdg-open'
