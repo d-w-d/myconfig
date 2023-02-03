4356782dd269d7db43cd55f7f953c14e5c2002b7
d-w-d <ddarg@umd.edu>
2023-02-03 09:33:32 -0500

    Fix nvm installation and initialization II

--- COMMON/common_nvm.sh
+++ COMMON/common_nvm.sh
@@ -1,12 +1,34 @@
 #!/usr/bin/env false
 
-# if [[ ! -d ~/.zsh-nvm ]]; then
-# git clone https://github.com/lukechilds/zsh-nvm.git ~/.zsh-nvm
-# fi
+if [ ! -x "$(command -v git)" ] && [ ! -x "$(command -v curl)" ] && _is_user_sudoer; then
+  sudo apt install -y git curl
+else
+  sleep 10
+  echo -e """${RED}
+    WARNING! Missing git and/or curl so can't install nvm!
+    This might mess up your vim installation and/or operation
+    since it depends on npm for typescript etc.
+  ${WHI}"""
+fi
 
-# Source zsh plugin even if we are using bash!
-# source ~/.zsh-nvm/zsh-nvm.plugin.zsh
+if [ -x "$(command -v git)" ] && [ -x "$(command -v curl)" ]; then
 
-# Enables nvm
-export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+  # Get nvm repo
+  if [[ ! -d ~/.nvm ]]; then
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+    _replace_dotrc_files
+  else
+    cd ~/.nvm
+    git pull
+  fi
+
+  # Enables nvm
+  export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+  # Make sure a version of node is installed
+  if ! [ -x "$(command -v node)" ]; then
+    nvm install 18
+  fi
+
+fi
