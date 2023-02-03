b8adaba5ad87a6cc8f2b656dc6e859e3e2442e61
d-w-d <ddarg@umd.edu>
2023-02-02 21:32:33 -0500

    New approach to nvm installation

--- COMMON/common_nvm.sh
+++ COMMON/common_nvm.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env false
 
-if [[ ! -d ~/.zsh-nvm ]]; then
-  git clone https://github.com/lukechilds/zsh-nvm.git ~/.zsh-nvm
-fi
+# if [[ ! -d ~/.zsh-nvm ]]; then
+# git clone https://github.com/lukechilds/zsh-nvm.git ~/.zsh-nvm
+# fi
 
 # Source zsh plugin even if we are using bash!
-source ~/.zsh-nvm/zsh-nvm.plugin.zsh
+# source ~/.zsh-nvm/zsh-nvm.plugin.zsh
+
+# Enables nvm
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
